4bdea2dec43a65eb656e79dad632c1db908e59cb
Leonardo Valões <leonardoberlandadevaloes@gmail.com>
2024-11-27 01:41:00 -0300

    fix profile

--- src/services/auth.js
+++ src/services/auth.js
@@ -8,16 +8,6 @@ async function verifyJWT(req, res, next) {
     return res.status(401).json({ error: "token não fornecido" });
   }
 
-  const tokenInBlackList = await prisma.blacklist.findUnique({
-    where: {
-      token: token,
-    },
-  });
-
-  if (tokenInBlackList) {
-    return res.status(401).json({ error: "permissão negada1" });
-  }
-
   jwt.verify(token, process.env.SECRET, (err, decoded) => {
     console.log(err);
     if (err) return res.status(401).json({ error: "permissão negada2" });
